ae440f101a4ea29f3ddf7b6571ec2993cf891e61
Xuan Nguyen <nguyenthihaxuan040900hiie@gmail.com>
2020-09-28 09:58:32 +0700

    dien_tich_hinh_chu_nhat

---
+++ canh_huyen_tam_giac_vuong.php
@@ -0,0 +1,59 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0-11/css/all.min.css">
+<link rel="stylesheet" href="style.css">
+<title>Cạnh huyền tam giác vuông</title>
+</head>
+<body>
+<div class="container">
+    
+    <form action="" method="POST" class="form-horizontal" role="form">
+            <div class="form-group">
+                <legend>CẠNH HUYỀN TAM GIÁC VUÔNG</legend>
+            </div>
+
+            <div class="form-group">                
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">Cạnh A</div>                
+                
+                <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">                    
+                    <input type="number" name="cạnh a" id="" class="form-control" value="" required="required">                   
+                </div>                
+            </div>
+
+            <div class="form-group">                
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">Cạnh B</div>                
+                
+                <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">                    
+                    <input type="number" name="cạnh b" id="" class="form-control" value="" required="required">                   
+                </div>                
+            </div>
+
+            <div class="form-group">                
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">Cạnh huyền</div>                
+                
+                <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10">                    
+                    <input type="number" name="cạnh huyền" id="" class="form-control" value="">                   
+                </div>                
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-10 col-sm-offset-2">
+                    <button type="submit" class="btn btn-primary">Tính</button>
+                </div>
+            </div>
+    </form>
+    
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
